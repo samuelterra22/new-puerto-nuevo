fd806dcd5333d448dce3a74ec754644d6b3ce21b
Samuel Terra <samuelterra22@hotmail.com>
2017-06-17 00:11:17 -0300

    Modelo de distribuição exponecial criado.

---
+++ src/br/edu/ifmg/simulacao/Exponencial.java
@@ -0,0 +1,19 @@
+package br.edu.ifmg.simulacao;
+
+public class Exponencial {
+
+    Double lambda;
+
+    public Exponencial(Double lambda) {
+        this.lambda = lambda;
+    }
+
+    public Double getLambda() {
+        return lambda;
+    }
+
+    public void setLambda(Double lambda) {
+        this.lambda = lambda;
+    }
+
+}
