ebd5cb2e3223e0a6df180c082c28f845ee50f7a7
Samuel Terra <samuelterra22@hotmail.com>
2017-06-16 23:37:56 -0300

    Commit inicial.

---
+++ src/br/edu/ifmg/simulacao/Main.java
@@ -0,0 +1,11 @@
+package br.edu.ifmg.simulacao;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("teste");
+
+    }
+}
---
+++ src/br/edu/ifmg/simulacao/Simulacao.java
@@ -0,0 +1,7 @@
+package br.edu.ifmg.simulacao;
+
+/**
+ * Created by samuel on 16/06/17.
+ */
+public class Simulacao {
+}
