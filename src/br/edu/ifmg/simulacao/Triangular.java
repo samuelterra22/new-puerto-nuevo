dcd3125a8957772096d7b95e5a3c2c1433be2851
Samuel Terra <samuelterra22@hotmail.com>
2017-06-17 00:10:28 -0300

    Modelo de distribuição triangular criado.

---
+++ src/br/edu/ifmg/simulacao/Triangular.java
@@ -0,0 +1,39 @@
+package br.edu.ifmg.simulacao;
+
+
+public class Triangular {
+
+    private Double a;
+    private Double b;
+    private Double c;
+
+    public Triangular(Double a, Double b, Double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Double getA() {
+        return a;
+    }
+
+    public void setA(Double a) {
+        this.a = a;
+    }
+
+    public Double getB() {
+        return b;
+    }
+
+    public void setB(Double b) {
+        this.b = b;
+    }
+
+    public Double getC() {
+        return c;
+    }
+
+    public void setC(Double c) {
+        this.c = c;
+    }
+}
