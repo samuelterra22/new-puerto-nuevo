0e1f8708ca2cfb9a60c80abd256a7d240e5e0b98
Samuel Terra <samuelterra22@hotmail.com>
2017-06-16 23:49:26 -0300

    Leitura do arquivo feita.

--- src/br/edu/ifmg/simulacao/Main.java
+++ src/br/edu/ifmg/simulacao/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("teste");
+        Simulacao s = new Simulacao();
+
+        s.leArquivo( System.getProperty("user.dir")+"/cenario.txt");
 
     }
 }
